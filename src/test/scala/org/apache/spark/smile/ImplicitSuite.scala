4c1ad1ada6f10510581013889580e1f50cfa8810
pierrenodet <nodet.pierre@gmail.com>
2019-07-27 02:38:20 +0200

    better coverage

--- src/test/scala/org/apache/spark/smile/ImplicitSuite.scala
+++ src/test/scala/org/apache/spark/smile/ImplicitSuite.scala
@@ -5,6 +5,7 @@ import org.scalatest.FunSuite
 import smile.classification.knn
 import smile.data._
 import smile.validation.{Accuracy, ClassificationMeasure, _}
+import org.apache.spark.sql.functions._
 
 class ImplicitSuite extends FunSuite with DatasetSuiteBase {
 
@@ -13,6 +14,20 @@ class ImplicitSuite extends FunSuite with DatasetSuiteBase {
     import org.apache.spark.smile.implicits._
 
     val mushrooms = spark.read.format("libsvm").load("data/mushrooms.svm")
+
+    val (x,y) = mushrooms.toSmileDataset().unzipInt
+
+    val res = cv(x, y, 5, Seq(new Accuracy().asInstanceOf[ClassificationMeasure]): _*) { (x, y) => knn(x, y, 3) }
+
+    assert(res(0) == 1)
+
+  }
+
+  test("toSmileDataset with weights") {
+
+    import org.apache.spark.smile.implicits._
+
+    val mushrooms = spark.read.format("libsvm").load("data/mushrooms.svm").withColumn("weight",lit(1.0))
 
     val (x,y) = mushrooms.toSmileDataset().unzipInt
 
